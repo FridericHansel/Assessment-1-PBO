29098452138f0273b658f38fcb2ff8acfb96f7c0
FridericHansel <71155596+FridericHansel@users.noreply.github.com>
2021-10-28 02:33:36 +0700

    Add files via upload

---
+++ Assessment 1 bagian 2/src/Mobil.java
@@ -0,0 +1,43 @@
+import java.text.DecimalFormat;
+
+public class Mobil {
+    private String merk,tahunKeluaran;
+    private Double harga;
+    private int stok;
+
+    Mobil (String merk , String tahunKeluaran , int stok , Double harga){
+        this.tahunKeluaran = tahunKeluaran;
+        this.merk = merk;
+        this.harga = harga;
+        this.stok= stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    public void setHarga(Double harga) {
+        this.harga = harga;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public String getTahunKeluaran() {
+        return tahunKeluaran;
+    }
+
+    public Double getHarga() {
+        return harga;
+    }
+
+    public Integer getStok() {
+        return stok;
+    }
+    public void displayInfo(){
+        DecimalFormat df = new DecimalFormat("########.#");
+        System.out.println();
+        System.out.println("Informasi Mobil" + "\nMerk : " +getMerk() +"\nHarga : " + df.format(getHarga())+"\nTahun Keluaran : "+getTahunKeluaran() + "\nSisa Stok : "+ getStok());
+    }
+}
